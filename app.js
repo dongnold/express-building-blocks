ddefd1956463863ebc6eddaf794501c33d81f1d1
dongnold <dongnold@gmail.com>
2018-01-21 16:58:22 +0100

    Seperate prod, dev and test databases

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlEncoder = bodyParser.urlencoded({extended:false});
+var redis = require('redis');
+var client = redis.createClient();
 
+client.select(process.env.NODE_ENV.length || "development".length);
+ 
 //app.get("/", function(request, response) {
 	//throw "Hey Error" ;
 //	response.json("OK");
@@ -13,22 +17,29 @@ var urlEncoder = bodyParser.urlencoded({extended:false});
 app.use(express.static("public"));
 
 
-var cities = {
-	"Lotopia": "lotopia descrption",
-	"Caspiana": "caspiana description", 
-	"Indigo" : "indigo description"
-	}; 
+client.hset("cities",  "Lotopia", "lotopia descrption"); 
+client.hset("cities",  "Caspiana", "caspiana description"); 
+client.hset("cities",  "Indigo" , "indigo description" ); 
+
 
 app.get("/cities", function(request, response) {
 	
-	response.json(Object.keys(cities));
+	
+	client.hkeys("cities", function(error, names) { 
+		if (error) throw error ;
+		response.json(names);
+	});
+	
 });
 
 
 app.post("/cities", urlEncoder, function(request, response) {
 	var newCity =  request.body ;
-	cities[newCity.name] = newCity.description;
-	response.status(201).json(newCity.name);
+	client.hset("cities", newCity.name, newCity.description,function(error){
+		if (error) throw error; 
+		response.status(201).json(newCity.name);
+	}) ;
+	
 });
 
 module.exports  = app ;
