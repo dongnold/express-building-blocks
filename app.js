269fb2144b0b7806841cae070a034798b63d0728
dongnold <dongnold@gmail.com>
2018-01-21 08:28:00 +0100

    Create new cities

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlEncoder = bodyParser.urlencoded({extended:false});
 
 //app.get("/", function(request, response) {
 	//throw "Hey Error" ;
@@ -10,9 +12,23 @@ var app = express();
 
 app.use(express.static("public"));
 
+
+var cities = {
+	"Lotopia": "lotopia descrption",
+	"Caspiana": "caspiana description", 
+	"Indigo" : "indigo description"
+	}; 
+
 app.get("/cities", function(request, response) {
-	var cities = ["Lotopia", "Caspiana", "Indigo"]; 
-	response.json(cities);
+	
+	response.json(Object.keys(cities));
+});
+
+
+app.post("/cities", urlEncoder, function(request, response) {
+	var newCity =  request.body ;
+	cities[newCity.name] = newCity.description;
+	response.status(201).json(newCity.name);
 });
 
 module.exports  = app ;
